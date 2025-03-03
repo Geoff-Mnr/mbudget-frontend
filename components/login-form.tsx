fef37babbfea49f2fc708bf960786d8bdb5fe1e5
Geoff Mnr <geoffrey_meunier@live.be>
2025-03-03 22:27:27 +0100

    Ajout de la redirection vers le tableau de bord lors de la connexion

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -1,12 +1,20 @@
+"use client";
+
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useRouter } from "next/navigation";
 
 export function LoginForm({
   className,
   ...props
 }: React.ComponentProps<"form">) {
+  const router = useRouter();
+  const handleLogin = () => {
+    router.push("/dashboard");
+  };
+
   return (
     <form className={cn("flex flex-col gap-6", className)} {...props}>
       <div className="flex flex-col items-center gap-2 text-center">
@@ -42,7 +50,7 @@ export function LoginForm({
             placeholder="********"
           />
         </div>
-        <Button type="submit" className="w-full">
+        <Button type="submit" className="w-full" onClick={handleLogin}>
           Se connecter
         </Button>
       </div>
