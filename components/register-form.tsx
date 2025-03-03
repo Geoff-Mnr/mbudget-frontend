e1b852ca6de0ab5d6463da25b84e6f7a02d3960f
Geoff Mnr <geoffrey_meunier@live.be>
2025-03-03 21:55:54 +0100

    Ajout du lien de redirection vers la page d'inscription

--- components/register-form.tsx
+++ components/register-form.tsx
@@ -3,19 +3,23 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
-export function LoginForm({
+export function RegisterForm({
   className,
   ...props
 }: React.ComponentProps<"form">) {
   return (
     <form className={cn("flex flex-col gap-6", className)} {...props}>
       <div className="flex flex-col items-center gap-2 text-center">
-        <h1 className="text-2xl font-bold ">Formulaire de connexion </h1>
+        <h1 className="text-2xl font-bold ">Formulaire d&apos;inscription </h1>
         <p className="text-muted-foreground text-sm text-balance">
           En route pour la gestion de votre budget 🚀
         </p>
       </div>
       <div className="grid gap-6">
+        <div className="grid gap-3">
+          <Label htmlFor="username">Nom d&apos;utilisateur</Label>
+          <Input id="username" type="text" placeholder="John Doe" required />
+        </div>
         <div className="grid gap-3">
           <Label htmlFor="email">Email</Label>
           <Input
@@ -28,12 +32,6 @@ export function LoginForm({
         <div className="grid gap-3">
           <div className="flex items-center">
             <Label htmlFor="password">Mot de passe</Label>
-            <a
-              href="#"
-              className="ml-auto text-sm underline-offset-4 hover:underline"
-            >
-              Mot de passe oublié ?
-            </a>
           </div>
           <Input
             id="password"
@@ -42,14 +40,25 @@ export function LoginForm({
             placeholder="********"
           />
         </div>
+        <div className="grid gap-3">
+          <div className="flex items-center">
+            <Label htmlFor="confirm_password">Confirmer le mot de passe</Label>
+          </div>
+          <Input
+            id="confirm_password"
+            type="password"
+            required
+            placeholder="********"
+          />
+        </div>
         <Button type="submit" className="w-full">
-          Se connecter
+          Créer un compte
         </Button>
       </div>
       <div className="text-center text-sm">
-        Pas de compte ?{" "}
-        <a href="/auth/register" className="underline underline-offset-4">
-          Créer un compte
+        Déjà un compte ?{" "}
+        <a href="/auth/login" className="underline underline-offset-4">
+          Se connecter
         </a>
       </div>
     </form>
